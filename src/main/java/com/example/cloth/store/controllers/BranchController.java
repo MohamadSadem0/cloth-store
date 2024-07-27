0b4947e055eaca5f65828d5b90fb91d2e306513e
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-07-27 18:07:13 +0300

    first controller working

--- src/main/java/com/example/cloth/store/controllers/BranchController.java
+++ src/main/java/com/example/cloth/store/controllers/BranchController.java
@@ -1,4 +1,18 @@
 package com.example.cloth.store.controllers;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"api/Brand"})
 public class BranchController {
+
+    @GetMapping
+    public ResponseEntity<String> helloworld(){
+    return new ResponseEntity<String>("hello from first controller", HttpStatus.OK);
+    }
 }
