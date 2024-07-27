dc1519dcf18b0a6ccc37d7b16ff9d144a419da5b
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-07-27 16:52:17 +0300

    .

---
+++ src/main/java/com/example/cloth/store/models/Branch.java
@@ -0,0 +1,17 @@
+package com.example.cloth.store.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="Branch")
+public class Branch {
+
+
+
+}
