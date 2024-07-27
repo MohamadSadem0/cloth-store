76bbf181b17409bd03edaad4d68a20f3e169da85
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-07-27 18:28:53 +0300

    .

--- src/main/java/com/example/cloth/store/controllers/BranchController.java
+++ src/main/java/com/example/cloth/store/controllers/BranchController.java
@@ -3,9 +3,7 @@ package com.example.cloth.store.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping({"api/Brand"})
@@ -13,6 +11,8 @@ public class BranchController {
 
     @GetMapping
     public ResponseEntity<String> helloworld(){
-    return new ResponseEntity<String>("hello from first controller", HttpStatus.OK);
+    return new ResponseEntity<String>("<h1>jwqhgeugeiqwi</h1>", HttpStatus.OK);
     }
+
+
 }
