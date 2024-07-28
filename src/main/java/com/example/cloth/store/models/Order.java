77165b611cb7af090b94073ff71d04733feb297e
HelmiAbouchakra <helmi.abouchakra@gmail.com>
2024-07-28 19:38:55 +0300

    Save3

--- src/main/java/com/example/cloth/store/models/Order.java
+++ src/main/java/com/example/cloth/store/models/Order.java
@@ -1,4 +1,61 @@
 package com.example.cloth.store.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "Order")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "OrderDate", nullable = false, updatable = false)
+    private Timestamp orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "UserID", nullable = false)
+    private User user;
+
+    @Column(name = "TotalAmount", nullable = false)
+    private BigDecimal totalAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "ShippingAddressID", nullable = false)
+    private Address shippingAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "BillingAddressID", nullable = false)
+    private Address billingAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "PaymentMethod", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "OrderStatus", nullable = false)
+    private OrderStatus orderStatus;
+
+    @Column(name = "TrackingNumber")
+    private String trackingNumber;
+
+    public enum PaymentMethod {
+        CREDIT_CARD, PAYPAL, BANK_TRANSFER
+    }
+
+    public enum OrderStatus {
+        PENDING, SHIPPED, DELIVERED, CANCELLED
+    }
 }
