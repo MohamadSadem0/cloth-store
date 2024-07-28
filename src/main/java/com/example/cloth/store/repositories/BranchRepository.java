cb1768d9ed03d698d5ad7c444cf78ab02741eb74
HelmiAbouchakra <helmi.abouchakra@gmail.com>
2024-07-28 21:35:12 +0300

    Save6

--- src/main/java/com/example/cloth/store/repositories/BranchRepository.java
+++ src/main/java/com/example/cloth/store/repositories/BranchRepository.java
@@ -1,5 +1,6 @@
 package com.example.cloth.store.repositories;
 
+
 import com.example.cloth.store.models.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
