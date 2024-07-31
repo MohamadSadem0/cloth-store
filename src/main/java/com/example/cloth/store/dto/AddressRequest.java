7177ae41a0c98ffbb74180fa3c41e6ebf938c8e1
HelmiAbouchakra <helmi.abouchakra@gmail.com>
2024-07-31 22:08:29 +0300

    address request fixed

--- src/main/java/com/example/cloth/store/dto/AddressRequest.java
+++ src/main/java/com/example/cloth/store/dto/AddressRequest.java
@@ -1,4 +1,26 @@
 package com.example.cloth.store.dto;
 
+import com.example.cloth.store.models.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class AddressRequest {
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private Address.AddressType addressType;
+
+    public enum AddressType {
+        SHIPPING, BILLING
+    }
 }
+
+
