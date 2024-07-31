d15bd60ef15c85483fd429568b5790451f7d8ef8
HelmiAbouchakra <helmi.abouchakra@gmail.com>
2024-07-31 22:01:33 +0300

    making address mapping

--- src/main/java/com/example/cloth/store/mappers/AddressMapper.java
+++ src/main/java/com/example/cloth/store/mappers/AddressMapper.java
@@ -1,5 +1,10 @@
 package com.example.cloth.store.mappers;
 
+import com.example.cloth.store.dto.AddressRequest;
+import com.example.cloth.store.models.Address;
+
 public class AddressMapper {
-    public sta
+    public static Address mapToAddress(AddressRequest addressRequest) {
+        return Address.builder().id(addressRequest.getId)
+    }
 }
