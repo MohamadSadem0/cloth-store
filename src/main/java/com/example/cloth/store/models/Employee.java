020ef73e604cca01215bd3527b00b18d5a0e8547
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-07-31 19:34:31 +0300

    models updated

---
+++ src/main/java/com/example/cloth/store/models/Employee.java
@@ -0,0 +1,48 @@
+package com.example.cloth.store.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "Employees")
+public class Employees {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EmployeeID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "BranchID", nullable = false)
+    private Branch branch;
+
+    @OneToOne
+    @JoinColumn(name = "UserID", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Role", nullable = false)
+    private EmployeeRole role;
+
+    @Column(name = "CreatedAt")
+    private Date createdAt;
+
+    @Column(name = "UpdatedAt")
+    private Date updatedAt;
+
+    @Column(name = "IsActive", nullable = false)
+    private Boolean isActive;
+
+    public enum EmployeeRole {
+        MANAGER, SELLER, CASHIER
+    }
+}
---
+++ src/main/java/com/example/cloth/store/models/Wishlist.java
@@ -0,0 +1,2 @@
+package com.example.cloth.store.models;public class Wishlist {
+}
