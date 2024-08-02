23d4e216afe6d131bc59b7e388588bfce1989886
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-08-02 15:41:58 +0300

    making the address controller

---
+++ src/main/java/com/example/cloth/store/controllers/AddressController.java
@@ -0,0 +1,8 @@
+package com.example.cloth.store.controllers;
+
+
+//todo: make api to get all properties of the address
+//todo: make api to add each property
+//todo: check when adding a new property if it already exists
+public class AddressController {
+}
