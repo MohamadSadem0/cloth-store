c9cf0264aca457b0eef331577224b8675e000d9b
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-08-02 16:36:51 +0300

    fix dir

---
+++ backend/src/main/java/com/example/cloth/store/controllers/AddressController.java
@@ -0,0 +1,17 @@
+package com.example.cloth.store.controllers;
+
+
+//todo: make apis to get all properties of the address
+//todo: make api to add each property
+//todo: check when adding a new property if it already exists
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/address")
+public class AddressController {
+
+}
